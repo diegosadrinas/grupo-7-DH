590c1f604f9af213add4605782468ebc9bcc46ab
MatiasDonati <matod87@hotmail.com>
2021-09-30 13:35:19 -0300

    rutas EJS

---
+++ src/routes/webRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const webController = require('../controllers/webControllers');
+
+
+router.get('/', webController.index);
+router.get('/login', webController.login);
+router.get('/register', webController.register);
+router.get('/productCart', webController.productCart);
+router.get('/productDetail', webController.productDetail);
+
+
+module.exports = router;
