a67fca7f3a9279581ad91370af1dc3c4ce727194
MatiasDonati <matod87@hotmail.com>
2021-09-30 13:34:42 -0300

    controllers modo EJS

---
+++ src/controllers/webControllers.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+
+
+module.exports ={
+    index: function (req, res){
+       return res.render(path.resolve(__dirname, '../views/index'));
+    },
+    login: function (req, res){
+       return res.render(path.resolve(__dirname, '../views/login'));
+    },
+    productCart: function (req, res){
+       return res.render(path.resolve(__dirname, '../views/productCart'));
+    },
+    productDetail: function (req, res){
+       return res.render(path.resolve(__dirname, '../views/productDetail'));
+    },
+    register: function (req, res){
+       return res.render(path.resolve(__dirname, '../views/register'));
+    },
+}
