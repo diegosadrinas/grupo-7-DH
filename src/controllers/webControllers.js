02ebb2005a0cc9eb11e46e034929c87121aafa51
MatiasDonati <matod87@hotmail.com>
2021-10-05 14:59:16 -0300

    webControllers.js con detalle/1 /2 /3

--- src/controllers/webControllers.js
+++ src/controllers/webControllers.js
@@ -1,6 +1,25 @@
 const path = require('path');
 
-
+const escritorios = [{
+   nombre: 'Escritorio 1',
+   descripcion: 'Cuando lugar de trabajo se transforma en tu lugar ideal, no hay Woodstack que pueda faltar en ese lugar',
+   precio: 'U$S 65.50',
+},
+   {
+   nombre: 'Escritorio 2',
+   descripcion: 'Cuando lugar de trabajo se transforma en tu lugar ideal, no hay Woodstack que pueda faltar en ese lugar',
+   precio: 'U$S 47.00',
+},  
+   {
+   nombre: 'Escritorio 3',
+   descripcion: 'Cuando lugar de trabajo se transforma en tu lugar ideal, no hay Woodstack que pueda faltar en ese lugar',
+   precio: 'U$S 27.50',
+},
+   {
+   nombre: 'Escritorio 4',
+   descripcion: 'Cuando lugar de trabajo se transforma en tu lugar ideal, no hay Woodstack que pueda faltar en ese lugar',
+   precio: 'U$S 37.50',
+}];
 
 module.exports ={
     index: function (req, res){
@@ -21,4 +40,8 @@ module.exports ={
     creacionProductos: function (req, res){
        return res.render(path.resolve(__dirname, '../views/creacionProdcutos'));
     },
+    detalle: function (req, res){
+      console.log(req.params)
+     return res.render(path.resolve(__dirname, '../views/productDetail'),{escritorios:escritorios[req.params.id]} );
+  },
 }
