a103877a1b9536a79b714a1dd06ab13791f1e284
MatiasDonati <matod87@hotmail.com>
2021-11-16 12:21:49 -0300

    validacion registro y base json

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const fs = require('fs');
+
+const usersFilePath = path.join(__dirname, '../data/usersDataBase.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const { validationResult } = require('express-validator')
 
@@ -18,15 +22,20 @@ const controller = {
 
 	processRegister: (req, res) => {
 		const resultValidation = validationResult(req);
-		
+      let newUser = {
+			id: users[users.length - 1].id + 1,
+			...req.body
+		};
+
 		if (resultValidation.errors.length > 0) {
 			return res.render('users/register', {
 				errors: resultValidation.mapped(),
 				oldData: req.body
 			});
 		}
-
-		return res.render('users/usuarioExito');
+      users.push(newUser)
+      fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2))
+		res.render('users/usuarioExito');
 	},
 
    profile: function (req, res){
