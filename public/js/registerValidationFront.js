45563f5c7889d655a7b380683f79899b60722b99
MatiasDonati <matod87@hotmail.com>
2022-01-20 19:52:49 -0300

    validacionFront falta arreglarlo

--- public/js/registerValidationFront.js
+++ public/js/registerValidationFront.js
@@ -5,41 +5,42 @@ const regExPassword = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-
 // !!!!! Acomodar todas las Class al Formulario !!!!!! 
 
 window.addEventListener("load", function(){
-    let formulario = document.querySelector("formRegister")
+    let formulario = document.querySelector(".form-register")
+
     formulario.addEventListener("submit", function(e){
         e.preventDefault()
 
         let errores = {}
         
         // Email
-        let campoEmail = document.querySelector("email-input")
+        let campoEmail = document.querySelector(".email-input")
         if (campoEmail.value == ""){
             errores.email = "Debes ingresar un Email"
-            document.querySelector("span-email").innerHTML = errores.email;
+            document.querySelector(".span-email").innerHTML = errores.email;
         }else if (regExEmail.test(campoEmail.value)){
             errores.email = "Debe ser un email valido"
-            document.querySelector("span-email").innerHTML = errores.email;
+            document.querySelector(".span-email").innerHTML = errores.email;
         } 
 
         // Nombre Formulario
-        let campoNombre = document.querySelector("nombre-input")
+        let campoNombre = document.querySelector(".nombre-input")
         if (campoNombre.value == ""){
             errores.nombre = "Debes ingresar un nombre"
-            document.querySelector("span-nombre").innerHTML = errores.nombre;
+            document.querySelector(".span-nombre").innerHTML = errores.nombre;
         }else if (campoNombre.value.length < 3){
             errores.nombre = "El nombre debe tener por lo menos dos caracteres"
-            document.querySelector("span-nombre").innerHTML = errores.nombre;
+            document.querySelector(".span-nombre").innerHTML = errores.nombre;
         } 
 
         // Apellido
-        let campoApellido = document.querySelector("apellido-input")
+        let campoApellido = document.querySelector(".apellido-input")
         if (campoApellido.value == ""){
             errores.apellido = "Debes ingresar un apellido"
-            document.querySelector("span-apellido").innerHTML = errores.apellido;
+            document.querySelector(".span-apellido").innerHTML = errores.apellido;
 
         }else if (campoApellido.value.length < 3){
             errores.apellido = "El apellido debe tener por lo menos tres caracteres"
-            document.querySelector("span-apellido").innerHTML = errores.apellido
+            document.querySelector(".span-apellido").innerHTML = errores.apellido
         } 
 
         // Password
@@ -47,11 +48,11 @@ window.addEventListener("load", function(){
         let campoPassword = document.querySelector(".apellido-input")
         if (campoPassword.value == ""){
             errores.password = "Debes ingresar una contraseña"
-            document.querySelector("span-password").innerHTML = errores.password;
+            document.querySelector(".span-password").innerHTML = errores.password;
 
         }else if (regExPassword.test(campoPassword)){
             errores.password = "Tu contraseña debe contener al menos 8 caracteres"
-            document.querySelector("span-password").innerHTML = errores.password;
+            document.querySelector(".span-password").innerHTML = errores.password;
         } 
 
         if (Object.keys(errores).length > 0) {
