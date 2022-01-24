788d0022fa45246dc2140d886898cbb646eb884f
Diego Sadrinas <dsadrinas@gmail.com>
2022-01-24 11:02:48 -0300

    subo cambios para las rutas

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -4,28 +4,6 @@ const db = require('../database/models');
 const products = []
 
 const controller = {
-<<<<<<< HEAD
-   index: (req, res) => {
-      res.render('products/all-products', {products})
-   },
-
-
-	//    TESTINGS !
-
-   test: (req, res)=>{
-	  res.render('products/testForm')
-   },
-   testPost: (req, res) => {
-	   let data = req.body
-	   console.log(data);
-	   res.send(data)
-   },
-	// FIN
-
-
-   create: (req, res) => {
-      res.render('products/product-create-form')
-=======
 	index: function (req, res){
 
 		db.Producto.findAll({
@@ -35,14 +13,13 @@ const controller = {
 		   })
 		   .catch(function(err){
   
-			  console.log("Error:" + String(err));
+			  console.log("Error en index:" + String(err));
 		  
 		  });
 	 },
    
 	create: (req, res) => {
 		res.render('products/product-create-form')
->>>>>>> 776078f8ea55a432eab24da884ebb9ac581c58aa
    },
 
 	cart: (req, res) => {
@@ -50,16 +27,12 @@ const controller = {
    },
 
 	store: (req, res) => {
-	   console.log("entré a store")
 		let image
-		console.log(req.files);
 		if(req.files[0] != undefined){
 			image = req.files[0].filename
 		} else {
 			image = 'stands-img.webp'
 		}
-
-		console.log(image, category, colors)
 		
 		db.Producto.create({
 			name: req.body.name,
@@ -71,19 +44,34 @@ const controller = {
 		})
 			.then(() =>
 				{
-					res.render('/products')
+					db.Producto.findAll({
+					})
+					   .then( (products) => {
+						  return res.render('products/all-products', {products});
+					   })
+					   .catch(function(err){
+			  
+						  console.log("Error en index:" + String(err));
+					  
+					  });
 			})
 
 			.catch(function(err){
 
-				console.log("Error:" + String(err));		
+				console.log("Error en Store:" + String(err));		
 			})	
    },
 
 	detail: (req, res) => {
 		let product_id = req.params.id
-		let product = products.find(product => product.id == product_id)
-		res.render('products/product-detail', {product});
+		db.Producto.findByPk(product_id)
+			.then((product => {
+				res.render('products/product-detail', {product});
+			}))
+			.catch((err) => {
+				console.log("Error en detail:" + String(err));	
+			})
+		
    },
 
 	edit: (req, res) => {
