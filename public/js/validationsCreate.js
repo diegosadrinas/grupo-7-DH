ec8ce07afaca5f5e05b9a959f712d9eb47278e09
MatiasDonati <matod87@hotmail.com>
2022-01-31 12:45:27 -0300

    valiaciones

--- public/js/validationsCreate.js
+++ public/js/validationsCreate.js
@@ -1,27 +1,29 @@
 const { body } = require('express-validator');
+const path = require('path')
 
 const validationsCreate = [
-    body('name')
+    body("name")
     .notEmpty().withMessage('Tienes que escribir un nombre').bail()
     .isLength({min:5}).withMessage('Debe terner al menos 5 caracteres'),
     body('description')
     .notEmpty().withMessage('Debes ingresar una descripción').bail()
-    .isLength({min:20}).withMessage('Debe terner al menos 5 caracteres'),
+    .isLength({min:20}).withMessage('Debe terner al menos 20 caracteres'),
     body('price')
     .notEmpty().withMessage('Debes ingresar un valor'),
-    body('image').custom((value, { req }) => {
-        let file = req.file;
-        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
-        if (!file) {
-            throw new Error ('Debes subir una imagen');
-        } else {
-            let fileExtension = path.extname(file.originalname);
-            if (!acceptedExtensions.includes(fileExtension)){
-                throw new Error ('El arhivo debe ser JPG, JPEG, PNG o GIF')
-            }
-        }
-        return true;
-    }),
+    // body('image').custom((value, { req }) => {
+    //     let file = req.files;
+    //     let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
+    //     if (!file) {
+    //         throw new Error ('Debes subir una imagen');
+    //     } else {
+    //         let fileExtension = path.extname(file.originalname);
+    //         console.log(fileExtension);
+    //         if (!acceptedExtensions.includes(fileExtension)){
+    //             throw new Error ('El arhivo debe ser JPG, JPEG, PNG o GIF')
+    //         }
+    //     }
+    //     return true;
+    // }),
     body('category').notEmpty().withMessage('Elegi una opcion'),
     body('color').notEmpty().withMessage('Elegi un color')
 ]
