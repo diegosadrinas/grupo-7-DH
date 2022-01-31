b268a9ed96fde813c367c4241e400595a59762c4
MatiasDonati <matod87@hotmail.com>
2022-01-31 15:58:49 -0300

    validacion producto

--- public/js/validationsCreate.js
+++ public/js/validationsCreate.js
@@ -10,20 +10,20 @@ const validationsCreate = [
     .isLength({min:20}).withMessage('Debe terner al menos 20 caracteres'),
     body('price')
     .notEmpty().withMessage('Debes ingresar un valor'),
-    // body('image').custom((value, { req }) => {
-    //     let file = req.files;
-    //     let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
-    //     if (!file) {
-    //         throw new Error ('Debes subir una imagen');
-    //     } else {
-    //         let fileExtension = path.extname(file.originalname);
-    //         console.log(fileExtension);
-    //         if (!acceptedExtensions.includes(fileExtension)){
-    //             throw new Error ('El arhivo debe ser JPG, JPEG, PNG o GIF')
-    //         }
-    //     }
-    //     return true;
-    // }),
+    body('image').custom((value, { req }) => {
+        let file = req.files;
+        let acceptedExtensions = ['.jpg', '.png', '.jpeg', '.gif'];
+        if (!file) {
+            throw new Error ('Debes subir una imagen');
+        // } else {
+        //     let fileExtension = path.extname(file.originalname);
+        //     console.log(fileExtension);
+        //     if (!acceptedExtensions.includes(fileExtension)){
+        //         throw new Error ('El arhivo debe ser JPG, JPEG, PNG o GIF')
+        //     }
+        }
+        return true;
+    }),
     body('category').notEmpty().withMessage('Elegi una opcion'),
     body('color').notEmpty().withMessage('Elegi un color')
 ]
